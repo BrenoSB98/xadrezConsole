86a5f530fdaefe7b70299fadd8e80d7ad070ad02
breno <brenosilvabraido1998@gmail.com>
2023-02-23 20:32:00 -0300

    Tratando erros com exceção personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,23 +7,28 @@ namespace xadrez_console{
     class Program { 
         static void Main(string[] args) {
 
-            Tabuleiro Tab = new(8, 8);
+            try {
+                Tabuleiro Tab = new(8, 8);
 
-            Tab.ColocarPeca(new Torre(Cor.Amarela, Tab), new Posicao(0, 0));
-            Tab.ColocarPeca(new Cavaleiro(Cor.Amarela, Tab), new Posicao(0, 1));
-            Tab.ColocarPeca(new Bispo(Cor.Amarela, Tab), new Posicao(0, 2));
-            Tab.ColocarPeca(new Rainha(Cor.Amarela, Tab), new Posicao(0, 3));
-            Tab.ColocarPeca(new Rei(Cor.Amarela, Tab), new Posicao(0, 4));
-            Tab.ColocarPeca(new Bispo(Cor.Amarela, Tab), new Posicao(0, 5));
-            Tab.ColocarPeca(new Cavaleiro(Cor.Amarela, Tab), new Posicao(0, 6));
-            Tab.ColocarPeca(new Torre(Cor.Amarela, Tab), new Posicao(0, 7));            
-           
-            Tab.ColocarPeca(new Torre(Cor.Amarela, Tab), new Posicao(7, 0));
-            Tab.ColocarPeca(new Torre(Cor.Amarela, Tab), new Posicao(7, 7));
+                Tab.ColocarPeca(new Torre(Cor.Amarela, Tab), new Posicao(0, 0));
+                Tab.ColocarPeca(new Cavaleiro(Cor.Amarela, Tab), new Posicao(0, 1));
+                Tab.ColocarPeca(new Bispo(Cor.Amarela, Tab), new Posicao(0, 2));
+                Tab.ColocarPeca(new Rainha(Cor.Amarela, Tab), new Posicao(0, 3));
+                Tab.ColocarPeca(new Rei(Cor.Amarela, Tab), new Posicao(0, 4));
+                Tab.ColocarPeca(new Bispo(Cor.Amarela, Tab), new Posicao(0, 5));
+                Tab.ColocarPeca(new Cavaleiro(Cor.Amarela, Tab), new Posicao(0, 6));
+                Tab.ColocarPeca(new Torre(Cor.Amarela, Tab), new Posicao(0, 7));
 
-            Tela.ImprimirTabuleiro(Tab);
+                Tab.ColocarPeca(new Torre(Cor.Amarela, Tab), new Posicao(7, 0));
+                Tab.ColocarPeca(new Torre(Cor.Amarela, Tab), new Posicao(7, 7));
 
+                Tela.ImprimirTabuleiro(Tab);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
             Console.WriteLine();
         }
+        
     }
 }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -15,9 +15,36 @@
             return _pecas[linhas, colunas];
         }
 
+        public Peca Peca(Posicao posicao) {
+            return _pecas[posicao.Linha, posicao.Coluna];
+        }
+
+        public bool ExistePecaNaPosicao(Posicao posicao) { 
+            ValidarPosicao(posicao);
+            return Peca(posicao) != null;
+        }
         public void ColocarPeca(Peca peca, Posicao posicao) {
+            if (ExistePecaNaPosicao(posicao)) {
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            }
             _pecas[posicao.Linha, posicao.Coluna] = peca;
             peca.Posicao = posicao;
+        }
+
+        public bool PosicaoValida(Posicao posicao) {
+            var PosLinha = posicao.Linha < 0 || posicao.Linha >= Linhas;
+            var PosColuna = posicao.Coluna < 0 || posicao.Coluna >= Colunas;
+            var PosFinal = PosLinha || PosColuna;
+            if (PosFinal) {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao posicao) {
+            if (!PosicaoValida(posicao)) {
+                throw new TabuleiroException("Posição Inválida");
+            }
         }
     }
 }
